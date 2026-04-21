d25113be6a92f90a43ab6c3ec55ba90331784865
Damian <125214141+TEB-DK@users.noreply.github.com>
2026-04-21 14:42:20 +0200

    Update index.php

--- 3. Zadania PHP/index.php
+++ 3. Zadania PHP/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Practice 🪄</title>
+    <link rel="icon" href="./favicon.svg">
     <style>
         * {
             margin: 0;
